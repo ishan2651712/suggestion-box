e291bd64d01545a0cd271c6e2d4e9b5efa65e71f
ishanmahajan2651712 <ishanmahajan2651712@gmail.com>
2025-12-14 18:37:02 +0530

    gitignorechanges

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", // 👈 Workbench username
-  password: "Tyujhg456", // 👈 Your password
-  database: "suggestion_box",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
